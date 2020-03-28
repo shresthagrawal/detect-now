944b668021e3e4b12c63d489772001debe2f81b2
mzkaramat <muhammadzeeshan020@gmail.com>
2020-03-28 10:31:30 +0100

    second attempt for app push

---
+++ App/app.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import time
+
+app = Flask(__name__)
+app.secret_key = 'super secret key'
+
+
+@app.route('/')
+def fn_landing():
+	return render_template('landing.html')
+
+
+@app.route('/sample')
+def fn_home():
+	return render_template('index.html')
+
+@app.route('/thanks')
+def fn_thanks():
+	return render_template('thanks.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def fn_upload():
+	if request.method == 'POST':
+		age = request.form.get("age",'na')
+		gender = request.form.get("gender",'na')
+		temperature = request.form.get("temperature",'na')
+		corona_test = request.form.get("corona_test",'na')
+		if 'audio_data' not in request.files:
+			flash('No file part')
+			return '400'
+		
+		file = request.files['audio_data']
+		ts = int(time.time())
+
+		filename = corona_test + '_' + temperature + '_' +gender + '_' +age + '_' + str(ts) + '.mp3'
+		file.save(os.path.join('upload', filename))
+
+		print(file.filename)
+
+	return '200'
+
+if __name__ == '__main__':
+	  app.run( port=8081, debug=True)
